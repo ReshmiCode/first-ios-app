c71ee42bd235c318cb6341fa6442d56893b0acbc
Reshmi Ranjith <rreshmiranjith@gmail.com>
2019-12-20 04:17:21 -0600

    optional features

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -10,16 +10,34 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    var backgroundColor: UIColor!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        backgroundColor = view.backgroundColor
     }
-
-
+    
+    
+    @IBAction func didTapStringButton(_ sender: Any) {
+        textLabel.text = textField.text
+        textField.text = ""
+        view.endEditing(true)
+        //textLabel.text = "Goodbye 👋"
+    }
+    
+    @IBAction func didTapViewButton(_ sender: Any) {
+        view.backgroundColor = UIColor.red
+    }
+    
     @IBAction func didTapButton(_ sender: Any) {
-        print("Hello")
         textLabel.textColor = UIColor.orange
     }
+    
+    @IBAction func onReset(_ sender: Any) {
+        textLabel.text = "Hello from Reshmi!"
+        view.backgroundColor = backgroundColor
+    }
+    
 }
 
